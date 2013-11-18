7d04d0169ac20ea2e214260b623b5947d4df460b
Scott Willeke <scott@willeke.com>
2013-11-17 23:33:35 -0800

    shell provisioning. added config to upload dir.

--- ubuntu-12.04.3-server-joomla/build.sh
+++ ubuntu-12.04.3-server-joomla/build.sh
@@ -2,6 +2,6 @@
 export PACKER_LOG=1
 export PACKER_LOG_PATH=./packer.log
 #NOTE: erb is from ruby. Tested with ruby 1.9.3p385
-erb packer.json.erb > ./packer.json
+erb packer.json.erb > ./packer.json.tmp
 
-packer build packer.json
+packer build packer.json.tmp
